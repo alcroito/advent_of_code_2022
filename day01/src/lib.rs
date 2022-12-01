434968dadecdef3bd9d2e4bbfcce2384e5568d92
alcroito <placinta@gmail.com>
2022-12-01 22:29:59 +0100

    initial

---
+++ day01/src/lib.rs
@@ -0,0 +1,18 @@
+use aoclib::parse;
+use std::path::Path;
+
+pub fn part1(input: &Path) -> Result<(), Error> {
+    unimplemented!("input file: {:?}", input)
+}
+
+pub fn part2(input: &Path) -> Result<(), Error> {
+    unimplemented!("input file: {:?}", input)
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+    #[error("no solution found")]
+    NoSolution,
+}
